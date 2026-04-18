8dfa991e5145970260ce7d43d41eff47126384c4
BacskaiKristof <bkristof1655@gmail.com>
2026-04-18 19:30:37 +0200

    Fő fájlok feltöltése

---
+++ includes/config.inc.php
@@ -0,0 +1,37 @@
+<?php
+$ablakcim = array(
+    'cim' => 'Lidi Travel Portál',
+);
+
+$fejlec = array(
+    'kepforras' => 'logo.png',
+    'kepalt' => 'logo',
+	'cim' => 'Lidi Travel Portál',
+	'motto' => ''
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'ceg' => 'Lidi Travel Portál'
+);
+
+$oldalak = array(
+	'/' => array('fajl' => 'fooldal', 'szoveg' => 'Főoldal', 'menun' => array(1,1)),
+	'szallasok' => array('fajl' => 'szallasok', 'szoveg' => 'Szállások', 'menun' => array(1,1)),
+	'adminreact' => array('fajl' => 'adminreact', 'szoveg' => 'Admin (React)', 'menun' => array(1,1)),
+	'jatekokspa' => array('fajl' => 'jatekokspa', 'szoveg' => 'Játékok (SPA)', 'menun' => array(1,1)),
+    'szerverajanlatok' => array('fajl' => 'szerverajanlatok', 'szoveg' => 'Szerver ajánlatok', 'menun' => array(1,1)),
+    'partnergaleriaaxios' => array('fajl' => 'partnergaleriaaxios', 'szoveg' => 'Partner Galéria', 'menun' => array(1,1)),
+    'flottakezelooojs' => array('fajl' => 'flottakezelooojs', 'szoveg' => 'Flotta Kezelő', 'menun' => array(1,1)),
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
+    'szalloda_crud' => array('fajl' => 'szalloda_crud', 'szoveg' => 'Szálloda Kezelő', 'menun' => array(1,1)),
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
+    'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
+    
+);
+
+
+
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
--- index.php
+++ index.php
@@ -1,68 +1,15 @@
 <?php
-session_start();
-
-// a) és b) Alapértelmezett oldal és jogosultság kezelés
-$oldal = isset($_GET['oldal']) ? $_GET['oldal'] : 'fooldal';
-
-// Kilépés kezelése
-if ($oldal == 'kilepes') {
-    session_destroy();
-    header("Location: index.php?oldal=fooldal");
-    exit();
-}
-
-$bejelentkezve = isset($_SESSION['user']);
-
-// e) pont: A kért formátum: Bejelentkezett: Családi_név Utónév (Login_név)
-$header_text = $bejelentkezve ? 
-    "Bejelentkezett: " . $_SESSION['user']['vnev'] . " " . $_SESSION['user']['knev'] . " (" . $_SESSION['user']['login'] . ")" : 
-    "Nincs bejelentkezve";
+	include('./includes/config.inc.php');
+	$oldal = $_SERVER['QUERY_STRING'];
+	if ($oldal!="") {
+		if (isset($oldalak[$oldal]) && file_exists("./templates/pages/{$oldalak[$oldal]['fajl']}.tpl.php")) {
+			$keres = $oldalak[$oldal];
+		}
+		else { 
+			$keres = $hiba_oldal;
+			header("HTTP/1.0 404 Not Found");
+		}
+	}
+	else $keres = $oldalak['/'];
+	include('./templates/index.tpl.php'); 
 ?>
-<!DOCTYPE html>
-<html lang="hu">
-<head>
-    <meta charset="UTF-8">
-    <title>Lidi Travel - Admin</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-
-<header>
-    <h1>Lidi Travel Portál</h1>
-    <div class="user-bar"><?php echo $header_text; ?></div>
-</header>
-
-<nav>
-    <ul>
-        <li><a href="index.php?oldal=fooldal">Főoldal</a></li>
-        <li><a href="index.php?oldal=kepek">Képek</a></li>
-        <li><a href="index.php?oldal=kapcsolat">Kapcsolat</a></li>
-        <li><a href="index.php?oldal=crud">CRUD</a></li>
-        
-        <?php if (!$bejelentkezve): ?>
-            <li><a href="index.php?oldal=auth" style="color:var(--accent)">Bejelentkezés</a></li>
-        <?php else: ?>
-            <li><a href="index.php?oldal=kilepes">Kilépés</a></li>
-        <?php endif; ?>
-    </ul>
-</nav>
-
-<main>
-    <?php
-    // Front-controller: fájlok betöltése a tartalom mappából
-    $fajl = "tartalom/$oldal.php";
-    if (file_exists($fajl)) {
-        include($fajl);
-    } else {
-        include("tartalom/fooldal.php");
-    }
-    ?>
-</main>
-
-<footer>
-    <p>Készítette: <strong>Budai Lili (GVLRPX)</strong> & <strong>Bácskai József Kristóf (GHX0DH)</strong></p>
-    <p>&copy; 2026 Lidi Travel - Minden jog fenntartva.</p>
-</footer>
-
-</body>
-</html>
